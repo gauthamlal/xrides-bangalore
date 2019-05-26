3edef7577de5ce96d3db0ef6c5521d11ca786586
Gautham Lal <gautham1995@gmail.com>
2019-05-26 15:21:44 +0530

    Made map responsive

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Dropzone from "react-dropzone";
 import csv from "csv";
 import ReactMapGL from "react-map-gl";
+// import { MapStylePicker } from "./controls";
 
 export default class Dashboard extends Component {
   state = {
@@ -16,6 +17,14 @@ export default class Dashboard extends Component {
       maxZoom: 16
     }
   };
+
+  componentDidMount() {
+    window.addEventListener("resize", this._resize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this._resize);
+  }
 
   handleDrop = files => {
     const file = files[0];
@@ -34,34 +43,69 @@ export default class Dashboard extends Component {
           });
           return rideObject;
         });
+        let totalLat = 0;
+        let totalLong = 0;
+        let totalCount = 0;
+        console.log(Number(rideList[0].from_lat));
+        rideList.forEach(item => {
+          // console.log(ite m.from_lat);
+          if (item.from_lat) {
+            // console.log(Number(item.from_lat));
+            totalLat += Number(item.from_lat);
+            totalLat = Math.round(totalLat * 100000) / 100000;
+            console.log(totalLat);
+            totalLong += Number(item.from_long);
+            totalLat = Math.round(totalLat * 100000) / 100000;
+            totalCount++;
+          }
+          if (item.to_lat) {
+            totalLat += Number(item.to_lat);
+            totalLong += Number(item.to_long);
+            totalCount++;
+          }
+        });
+        console.log(totalLat);
+        const viewPortLat = totalLat / totalCount;
+        const viewPortLong = totalLong / totalCount;
+        console.log(viewPortLat, viewPortLong);
         this.setState({ rideList });
       });
     };
     reader.readAsBinaryString(file);
   };
 
+  _onViewportChange = viewport => {
+    this.setState({ viewport: { ...this.state.viewport, ...viewport } });
+  };
+
+  _resize = () => {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
   render() {
     console.log(this.state);
     return (
       <div>
-        <div className="dropzone">
-          <Dropzone
-            accept=".csv"
-            onDrop={acceptedFiles => this.handleDrop(acceptedFiles)}
-          >
-            {({ getRootProps, getInputProps }) => (
-              <section>
-                <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  <p>Drag 'n' drop some files here, or click to select files</p>
-                </div>
-              </section>
-            )}
-          </Dropzone>
-        </div>
+        <Dropzone
+          accept=".csv"
+          onDrop={acceptedFiles => this.handleDrop(acceptedFiles)}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <section className="dropzone">
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+
         <ReactMapGL
           {...this.state.viewport}
-          onViewportChange={viewport => this.setState({ viewport })}
+          onViewportChange={viewport => this._onViewportChange(viewport)}
           mapStyle={this.state.style}
         />
       </div>
