78d934b4e7e8a9658d803671852b4316c8164976
Gautham Lal <gautham1995@gmail.com>
2019-06-01 00:50:03 +0530

    removed redux dev tool

--- src/store.js
+++ src/store.js
@@ -5,12 +5,6 @@ import rootReducer from "./reducers/rootReducer";
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
