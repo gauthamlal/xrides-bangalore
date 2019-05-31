b95b39abec63b3c28e2a827bfa7609cde06d71d9
Gautham Lal <gautham1995@gmail.com>
2019-05-31 21:37:34 +0530

    Design changes

--- src/components/layout/NavbarComponent.js
+++ src/components/layout/NavbarComponent.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function NavbarComponent() {
   return (
     <div className="navbar">
+      <h2>X-RIDES</h2>
       <div className="left">
         <img src="/logo.png" alt="logo" />
       </div>
 
-      <h2>X-RIDES</h2>
       {/* <div className="right" /> */}
     </div>
   );
