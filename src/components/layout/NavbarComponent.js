904fe89cd41be7a28f9f3c919606057c0209b75e
Gautham Lal <gautham1995@gmail.com>
2019-06-01 04:50:09 +0530

    navbar title change

--- src/components/layout/NavbarComponent.js
+++ src/components/layout/NavbarComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavbarComponent() {
   return (
     <div className="navbar">
-      <h2>X-RIDES</h2>
+      <h2>XRides</h2>
       <div className="left">
         <img src="/logo.png" alt="logo" />
       </div>
